738a36ffb5da3aa9fa32ee358351ab87df0af30c
Eric Almeida <eric.rva@gmail.com>
2017-11-30 15:14:55 -0200

    login

--- app/src/main/java/com/example/meucomputador/helloworld/BemVindoActivity.java
+++ app/src/main/java/com/example/meucomputador/helloworld/BemVindoActivity.java
@@ -2,6 +2,7 @@ package com.example.meucomputador.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BemVindoActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class BemVindoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bem_vindo);
+
+        //Recebe o nome enviado por parâmetro
+        Bundle args = getIntent().getExtras();
+        String nome = args.getString("nome");
+
+        //Atualiza o texto do TextView com uma mensagem de bem-vindo
+        TextView text = (TextView) findViewById(R.id.text);
+        text.setText(nome + " seja bem-vindo");
     }
 }
--- app/src/main/java/com/example/meucomputador/helloworld/MainActivity.java
+++ app/src/main/java/com/example/meucomputador/helloworld/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.meucomputador.helloworld;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btLogin = (Button)findViewById(R.id.btLogin);
+        btLogin.setOnClickListener(onClickLogin());
+    }
+
+    private View.OnClickListener onClickLogin() {
+        return new View.OnClickListener(){
+            public void onClick(View v){
+                TextView tLogin = (TextView)findViewById(R.id.tLogin);
+                TextView tPass = (TextView)findViewById(R.id.tPass);
+                String login = tLogin.getText().toString();
+                String pass = tPass.getText().toString();
+
+                if (login.equals("Eric") && (pass.equals("123"))){
+                    //Navega para a próxima tela
+                    Intent intent = new Intent(getContext(), BemVindoActivity.class);
+                    Bundle params = new Bundle();
+                    params.putString("nome", "Eric Almeida");
+                    intent.putExtras(params);
+                    startActivity(intent);
+                } else {
+                    alert("Login e/ou senha incorretos");
+                }
+            }
+        };
+    }
+
+    private Context getContext() {
+        return this;
+    }
+
+    private void alert(String s) {
+        Toast.makeText(this,s,Toast.LENGTH_SHORT).show();
     }
 }
